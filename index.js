2209566d1ee1e39505ae54d0f33be12805fd4ef7
Tyler Gilchrist <tyler668@gmail.com>
2019-10-14 13:56:17 -0400

    attempt at auth middleware and restricted route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const restricted = require('./user-auth.js')
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
@@ -19,6 +20,10 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
+  const hash = bcrypt.hashSync(user.password, 12)
+
+  user.password = hash;
+
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -30,28 +35,44 @@ server.post('/api/register', (req, res) => {
 
 server.post('/api/login', restricted, (req, res) => {
   let { username, password } = req.body;
-
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'You cannot pass!!' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(400).json({ message: 'please provide credentials' });
+  }
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+server.get('/hash', (req, res) => {
+  const password = req.headers.authorization;
+  if (password) {
+    const hash = bcrypt.hashSync(password, 10); //This number is the complexity of the encryption
+
+    res.status(200).json({ hash });
+  } else {
+    res.status(400).json({ message: 'Provide Valid Credentials' });
+  }
+
+
+})
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
